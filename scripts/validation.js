252c16c3aaba739c7dea925c85cd92a22e1255fc
Altin Morina <altinmorina2002@gmail.com>
2023-01-08 05:14:36 +0100

    Main Page Added + Small Feature
    Added a video content as a main page to the content,also changed some small features on the slider and the responsive side.

--- scripts/validation.js
+++ scripts/validation.js
@@ -10,55 +10,46 @@ form.onsubmit = (e) => {
   pInput.value == "" ? pField.classList.add("shake", "error") : checkPass();
 
   setTimeout(() => {
-    //remove shake class after 500ms
     eField.classList.remove("shake");
     pField.classList.remove("shake");
   }, 500);
 
   eInput.onkeyup = () => {
     checkEmail();
-  }; //calling checkEmail function on email input keyup
+  };
   pInput.onkeyup = () => {
     checkPass();
-  }; //calling checkPassword function on pass input keyup
+  };
 
   function checkEmail() {
-    //checkEmail function
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     if (!eInput.value.match(pattern)) {
-      //if pattern not matched then add error and remove valid class
       eField.classList.add("error");
       eField.classList.remove("valid");
       let errorTxt = eField.querySelector(".error-txt");
-      //if email value is not empty then show please enter valid email else show Email can't be blank
       eInput.value != ""
         ? (errorTxt.innerText = "Enter a valid email address")
         : (errorTxt.innerText = "Email can't be blank");
     } else {
-      //if pattern matched then remove error and add valid class
       eField.classList.remove("error");
       eField.classList.add("valid");
     }
   }
 
   function checkPass() {
-    //checkPass function
     if (pInput.value == "") {
-      //if pass is empty then add error and remove valid class
       pField.classList.add("error");
       pField.classList.remove("valid");
     } else {
-      //if pass is empty then remove error and add valid class
       pField.classList.remove("error");
       pField.classList.add("valid");
     }
   }
 
-  //if eField and pField doesn't contains error class that mean user filled details properly
   if (
     !eField.classList.contains("error") &&
     !pField.classList.contains("error")
   ) {
-    window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
+    window.location.href = form.getAttribute("action");
   }
 };

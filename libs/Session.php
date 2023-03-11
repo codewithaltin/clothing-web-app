871113ebe167bc03cf81991ef7670cbb19722b14
Altin Morina <altinmorina2002@gmail.com>
2023-03-11 16:26:40 +0100

    Set,Get,Clear,Destroy Session Functions

--- libs/Session.php
+++ libs/Session.php
@@ -4,5 +4,22 @@ class Session{
     public static function start(){
         session_start();
     }
+    public static function set($key,$value){
+        $_SESSION[$key]=$value;
+    }
 
+    public static function get($key){
+        if(isset($_SESSION[$key]))
+            return $_SESSION[$key];
+        else
+            return null;
+    }
+    
+    public static function clear($key){
+        unset($_SESSION[$key]);
+    }
+
+    public static function destroy(){
+        session_destroy();
+    }
 }

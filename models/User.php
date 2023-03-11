f06d7e910df8a6be9db9c9d018e8906a61cbaa7a
Altin Morina <altinmorina2002@gmail.com>
2023-03-11 15:31:03 +0100

    User Class Added

---
+++ models/User.php
@@ -0,0 +1,24 @@
+<?php
+
+
+include_once (__DIR__ .'/../libs/BaseModel.php');
+
+class User extends BaseModel{
+    
+    private $id;
+    public $emri;s
+    public $email;
+    public $password;
+    public $roli=1;
+    public $id_dyqani=1;
+
+    function __construct(array $user=[]){
+        parent::__construct();
+
+        $this->emri=isset($user['emri'])?$user['emri']:null;
+        $this->email=isset($user['email'])?$user['email']:null;
+        $this->password=isset($user['password'])?$user['password']:null;
+        $this->roli=isset($user['roli'])?$user['roli']:null;
+        $this->id_dyqani=isset($user['id_dyqani'])?$user['id_dyqani']:null;
+    }
+}
